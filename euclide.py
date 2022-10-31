42a232d374ba47ea96e9c2c436527894ba92d999
BenjaminCoupry <56168210+BenjaminCoupry@users.noreply.github.com>
2022-10-31 21:29:01 +0100

    euclide

---
+++ euclide.py
@@ -0,0 +1,20 @@
+
+from re import A
+
+
+def init_vars(a,b):
+    r1 = b
+    r2 = a
+    u1 = 0
+    v1 = 1
+    u2 = 1
+    v2 = 0
+    return r1,r2,u1,v1,u2,v2
+
+def extended_euclide(a,b):
+    r1,r2,u1,v1,u2,v2 = init_vars(a,b)
+    while r2 !=0:
+        q = r1//r2
+        r1,u1,v1,r2,u2,v2 = r2,u2,v2,r1-q*r2,u1-q*u2,v1-q*v2
+        print(r1,u1,v1)
+    return r1,u1,v1
