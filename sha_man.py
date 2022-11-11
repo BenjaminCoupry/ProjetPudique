3a3730fee20bcb94184d015dd0b400621b0fece5
Yannis Mestre <Yannis.Mestre@grenoble-inp.org>
2022-11-11 21:30:40 +0100

    SHA-256 qui semble OK mais qui donne pas le bon hash.

--- sha_man.py
+++ sha_man.py
@@ -14,10 +14,61 @@ def bourrage(data):
     if l % 512 == 0 and l != 0:
         return []
     k = (448 - l - 1) % 512
-    padding = [0b1000000]
+    padding = [0b10000000]
     padding += ((k-7) // 8) * [0x00]
     mask = 0xFF << 56
     for i in range(8):
         padding.append( (l & mask) >> 8 * (7-i) )
         mask >>= 8
     return padding
+
+
+def build_W(block):
+    '''
+    Renvoie le tableau W construit à partir du bloc.
+    block : Un bloc, 64 octets dans un tableau.
+    '''
+    W = [ block[i]*256**3 + block[i+1]*256**2 + block[i+2]*256 + block[i+3] for i in range(0, 64, 4) ]
+    for t in range(16, 64):
+        W.append( (sp.small_sigma_1_256(W[t-2]) + W[t-7] + sp.small_sigma_0_256(W[t-15]) + W[t-16]) % sp.U32_MAX )
+    print("W =", W)
+    return W
+
+
+def compute_intermediate_hashes(W, H):
+    '''
+    Calcule les hashs intermédiaires pour le W donné et le H du bloc précédent.
+    Renvoie a, b, c, d, e, f, g, h sous forme de tuple
+    '''
+    a, b, c, d, e, f, g, h = H
+    for t in range(64):
+        t1 = h + sp.big_sigma_1_256(e) + sp.ch(e, f, g) + sp.K_256[t] + W[t]
+        t2 = sp.big_sigma_0_256(a) + sp.maj(a, b, c)
+        h, g, f, e, d, c, b, a = g, f, e, (d+t1) % sp.U32_MAX, c, b, a, (t1+t2) % sp.U32_MAX
+    return a, b, c, d, e, f, g, h
+
+
+def sha_256_sum(data):
+    '''
+    Renvoie le hash SHA-256 des données (tableau d'octets)
+    data : tableau d'octets (entiers).
+    '''
+    H = [h for h in sp.H_0]
+    padded_data = data + bourrage(data)
+    print(padded_data, "\n\n")
+    for i in range(len(padded_data) // 64):
+        block = padded_data[i:i+64]
+        W = build_W(block)
+        intermediate_H = compute_intermediate_hashes(W, H)
+        print(intermediate_H)
+        H = [ (h + ih) % sp.U32_MAX for h, ih in zip(H,intermediate_H) ]
+        print(H, "\n\n")
+    return [ item for subl in map(lambda h : ((0xFF000000 & h) >> 24, (0xFF0000 & h) >> 16, (0xFF00 & h) >> 8, 0xFF & h) , H ) for item in subl ]
+
+
+data = [ord(c) for c in "BONJOUR"]
+s = sha_256_sum(data)
+for b in s:
+    print("{:02x}".format(b), end='')
+print()
+
